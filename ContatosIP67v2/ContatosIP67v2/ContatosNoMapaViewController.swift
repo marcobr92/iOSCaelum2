a5bb2c5cbd2e90f00fc8ea231c7cfd34d83610cd
ios8106 <ios8106@mac-mini-3.local>
2019-03-25 20:01:23 -0300

    exercício 15.3

---
+++ ContatosIP67v2/ContatosIP67v2/ContatosNoMapaViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ContatosNoMapaViewController.swift
+//  ContatosIP67v2
+//
+//  Created by ios8106 on 25/03/19.
+//  Copyright © 2019 Caelum. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class ContatosNoMapaViewController: UIViewController {
+    @IBOutlet weak var mapa: MKMapItem!
+}
